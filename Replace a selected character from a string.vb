b3f64c3949040c725433cde7a99c72b9352e1d76
Hassans-hub <119284894+Hassans-hub@users.noreply.github.com>
2022-12-02 12:20:15 +0500

    Add files via upload

---
+++ Replace a selected character from a string.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String
+        Dim char1 As String
+        Dim newstr As String
+        Dim pos As Integer
+
+        Console.Write("Enter a string: ")
+        str = Console.ReadLine
+        Console.Write("Enter the position of the character you wish to replace: ")
+        pos = Console.ReadLine
+
+        If pos > Len(str) Then
+            Console.Write("You entered an invalid positon that doesnt exist")
+        End If
+
+        Console.Write("Which character you want to replace it with: ")
+        char1 = Console.ReadLine
+
+        For i = 1 To Len(str)
+            If i <> pos Then
+                newstr = newstr & Mid(str, i, 1)
+            Else
+                newstr = newstr & char1
+            End If
+        Next
+
+        Console.Write("Your modified string: " & newstr)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ check if string has all alphabets.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String
+        Dim Alpha As String
+        Dim Flag1 As Boolean
+        Dim chaar As String
+        Dim char2 As String
+
+        Flag1 = True
+        Console.Write("Enter a sentence: ")
+        str = Console.ReadLine
+
+        Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        str = UCase(str)
+
+        For i = 1 To Len(Alpha)
+            chaar = Mid(Alpha, i, 1)
+            char2 = InStr(str, chaar)
+            If char2 < 1 Then Flag1 = False
+        Next
+        If Flag1 = True Then
+            Console.Write("Your string contains all alphabets")
+        Else
+            Console.Write("Your string does not contain all alphabets")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
