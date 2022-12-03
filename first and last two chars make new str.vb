3fde83f942071cf6628c26037bbfca2261b4f542
Hassans-hub <119284894+Hassans-hub@users.noreply.github.com>
2022-12-03 13:53:26 +0500

    Add files via upload

---
+++ first and last two chars make new str.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s As String
+        Dim newstr As String
+        Dim leftside As String
+        Dim rightside As String
+
+        Console.Write("Enter a string: ")
+        s = Console.ReadLine
+        If Len(s) <= 2 Then
+            Console.Write("Your string length was too short" & vbCrLf & s)
+            Console.ReadKey()
+        Else
+            leftside = Left(s, 2)
+            rightside = Right(s, 2)
+            newstr = leftside & rightside
+            Console.Write("Your new string is: " & newstr)
+            Console.ReadKey()
+
+        End If
+    End Sub
+
+End Module
