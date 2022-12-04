200fd0777aa95b11af6dde1d14487b11b5036e5f
Hassans-hub <119284894+Hassans-hub@users.noreply.github.com>
2022-12-04 08:48:55 +0500

    Add files via upload

---
+++ replace first letter occurences with staric.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s As String
+        Dim char1 As Char
+        Dim char2 As Char
+        Dim newstr As String
+
+        Console.Write("Enter a string: ")
+        s = Console.ReadLine
+
+        s = UCase(s)
+        char1 = Mid(s, 1, 1)
+        newstr = newstr & char1
+
+        For i = 2 To Len(s)
+            char2 = Mid(s, i, 1)
+            If char2 = char1 Then
+                newstr = newstr & "*"
+            Else
+                newstr = newstr & char2
+            End If
+        Next
+
+        Console.Write("Your new string is: " & newstr)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
