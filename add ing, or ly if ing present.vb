1a5dd8a24aed89e250654c559e560ec07a5243c3
Hassans-hub <119284894+Hassans-hub@users.noreply.github.com>
2022-12-04 09:55:27 +0500

    Add files via upload
    Given a string, if its length is at least 3,
    add 'ing' to its end.
    Unless it already ends in 'ing', in which case
    add 'ly' instead.
    If the string length is less than 3, leave it unchanged.
    Return the resulting string.
    examples:
    input 'end' --> 'ending'
    input 'ending' --> 'endingly'
    input 'go' --> 'go'

---
+++ add ing, or ly if ing present.vb
@@ -0,0 +1,48 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String
+        Dim ing As String
+        Dim char1 As Char
+        Dim char2 As Char
+        Dim c As Integer
+        Dim flag As Boolean
+
+        ing = "ing"
+        Console.Write("Enter a string: ")
+        str = Console.ReadLine
+
+        If Len(str) <= 3 Then
+            Console.Write("Your entered string should be at least 3 characters in length" & vbCrLf)
+            Console.Write(str)
+            Console.ReadKey()
+        Else
+
+            For i = (Len(str) - 2) To Len(str)
+                c = c + 1
+                char1 = Mid(str, i, 1)
+                char2 = Mid(ing, c, 1)
+
+                If char1 <> char2 Then
+                    flag = False
+                Else
+                    flag = True
+                End If
+
+            Next
+
+            If flag = False Then
+                str = str & "ing"
+            Else
+                str = str & "ly"
+            End If
+
+            Console.Write("Your new string is: " & str)
+
+            Console.ReadKey()
+
+        End If
+
+    End Sub
+
+End Module
