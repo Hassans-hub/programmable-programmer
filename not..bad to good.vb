4a97b74b90692d089be089b4cf79e39cf7c3ee70
Hassans-hub <119284894+Hassans-hub@users.noreply.github.com>
2022-12-04 10:18:29 +0500

    Add files via upload
    Given a string, find the first appearance of the
    substring 'not' and 'bad'. If the 'bad' follows
    the 'not', replace the whole 'not'...'bad' substring
    with 'good'.
    Return the resulting string.
    Input: 'This dinner is not that bad!' 
    Outputs: This dinner is good!

---
+++ not..bad to good.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String
+        Dim notnum As Integer
+        Dim badnum As Integer
+        Dim char1 As Char
+        Dim newstr As String
+
+
+        Console.Write("Enter a string: ")
+        str = Console.ReadLine
+
+        notnum = InStr(str, "not")
+        badnum = InStr(str, "bad")
+
+        If notnum > 0 And badnum > 0 And notnum < badnum Then
+
+            For i = 1 To notnum - 1
+                char1 = Mid(str, i, 1)
+                newstr = newstr & char1
+            Next
+
+            newstr = newstr & "good"
+            Console.Write(newstr)
+            Console.ReadKey()
+
+        Else
+
+            Console.Write(str)
+            Console.ReadKey()
+
+        End If
+
+
+
+
+    End Sub
+
+End Module
